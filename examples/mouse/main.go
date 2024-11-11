173abf07f3055baa112a4d6039dc3e4bb1fc2824
noppikinatta <noppikitouroku@gmail.com>
2024-11-12 06:50:58 +0900

    ✨ add virtual mouse

--- examples/mouse/main.go
+++ examples/mouse/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/noppikinatta/nyuuryoku"
 )
 
@@ -26,6 +27,8 @@ type game struct {
 	log              []string
 	logIdx           int
 	pressedDurations map[ebiten.MouseButton]int
+	mouseIsVirtual   bool
+	virtualMouse     *virtualMouse
 }
 
 func (g *game) Update() error {
@@ -35,6 +38,17 @@ func (g *game) Update() error {
 	}
 	if g.keyboard == nil {
 		g.keyboard = nyuuryoku.NewKeyboard()
+	}
+	if g.virtualMouse == nil {
+		g.virtualMouse = newVirtualMouse()
+	}
+
+	if g.mouseIsVirtual {
+		g.virtualMouse.Update()
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		g.switchMouse()
 	}
 
 	g.appendButtonLog(ebiten.MouseButtonLeft)
@@ -82,7 +96,7 @@ func (g *game) appendWheelLog() {
 }
 
 func (g *game) appendLog(line string) {
-	const max = 50
+	const max = 48
 	if len(g.log) < max {
 		g.log = append(g.log, line)
 	} else {
@@ -90,6 +104,22 @@ func (g *game) appendLog(line string) {
 		g.logIdx++
 		g.logIdx = g.logIdx % max
 	}
+}
+
+func (g *game) switchMouse() {
+	setter := nyuuryoku.MouseSetter{Mouse: g.mouse}
+
+	if g.mouseIsVirtual {
+		setter.SetDefault()
+	} else {
+		setter.SetPressedFunc(g.virtualMouse.buttonPressed)
+		setter.SetJustPressedFunc(g.virtualMouse.buttonJustPressed)
+		setter.SetJustReleasedFunc(g.virtualMouse.buttonJustReleased)
+		setter.SetPressDurationFunc(g.virtualMouse.pressDuration)
+		setter.SetWheelFunc(g.virtualMouse.Wheel)
+	}
+
+	g.mouseIsVirtual = !g.mouseIsVirtual
 }
 
 func (g *game) Draw(screen *ebiten.Image) {
@@ -113,6 +143,8 @@ func (g *game) Draw(screen *ebiten.Image) {
 
 		ebitenutil.DebugPrintAt(screen, line, 200, y)
 	}
+
+	ebitenutil.DebugPrintAt(screen, "press space to switch to virtual mouse", 10, 576)
 }
 
 func (g *game) drawButton(screen *ebiten.Image, bounds image.Rectangle, button ebiten.MouseButton) {
@@ -125,6 +157,67 @@ func (g *game) drawButton(screen *ebiten.Image, bounds image.Rectangle, button e
 	}
 
 	screen.DrawImage(dummyWhitePixel, &opt)
+}
+
+type virtualMouse struct {
+	intervals     map[ebiten.MouseButton]int
+	wheelInterval int
+	count         int
+}
+
+func newVirtualMouse() *virtualMouse {
+	return &virtualMouse{
+		intervals: map[ebiten.MouseButton]int{
+			ebiten.MouseButtonLeft:   60,
+			ebiten.MouseButtonRight:  90,
+			ebiten.MouseButtonMiddle: 150,
+		},
+		wheelInterval: 21,
+	}
+}
+
+func (m *virtualMouse) Update() {
+	m.count++
+}
+
+func (m *virtualMouse) buttonPressed(button ebiten.MouseButton) bool {
+	interval := m.intervals[button]
+	quotient := m.count / interval
+	return quotient%2 == 1
+}
+
+func (m *virtualMouse) buttonJustPressed(button ebiten.MouseButton) bool {
+	if !m.buttonPressed(button) {
+		return false
+	}
+	return m.duration(button) == 1
+}
+
+func (m *virtualMouse) buttonJustReleased(button ebiten.MouseButton) bool {
+	if m.buttonPressed(button) {
+		return false
+	}
+	return m.duration(button) == 1
+}
+
+func (m *virtualMouse) pressDuration(button ebiten.MouseButton) int {
+	if !m.buttonPressed(button) {
+		return 0
+	}
+	return m.duration(button)
+}
+
+func (m *virtualMouse) duration(button ebiten.MouseButton) int {
+	interval := m.intervals[button]
+	return m.count%interval + 1
+}
+
+func (m *virtualMouse) Wheel() (xoff, yoff float64) {
+	quotient := m.count / m.wheelInterval
+	if quotient%10 == 1 {
+		return 0.1, 0.1
+	}
+	return 0, 0
 }
 
 func (g *game) Layout(outsideWidth int, outsideHeight int) (screenWidth int, screenHeight int) {
