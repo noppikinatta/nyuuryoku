0415457a16970a8e0b90a0d1aea44e8f3074fe94
noppikinatta <noppikitouroku@gmail.com>
2025-02-26 08:51:56 +0900

    ✨ Add gamepad example with comprehensive input display

--- examples/gamepad/main.go
+++ examples/gamepad/main.go
@@ -2,234 +2,141 @@ package main
 
 import (
 	"fmt"
-	"image"
 	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/noppikinatta/nyuuryoku"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 )
 
 const (
-	screenW = 800
-	screenH = 600
+	screenWidth  = 640
+	screenHeight = 480
 )
 
-func main() {
-	ebiten.SetWindowSize(screenW, screenH)
-
-	g := game{}
-	if err := ebiten.RunGame(&g); err != nil {
-		log.Fatal(err)
-	}
+type Game struct {
+	gamepad   *nyuuryoku.Gamepad
+	mplusFont font.Face
 }
 
-type game struct {
-	conn           *nyuuryoku.GamepadConnection
-	gamepad        *nyuuryoku.StandardGamepad
-	connIsVirtual  bool
-	virtualConn    *virtualGamepadConnection
-	connectedIDs   []ebiten.GamepadID
-	names          map[ebiten.GamepadID]string
-	pressedButtons map[ebiten.GamepadID][]ebiten.StandardGamepadButton
-	axisValues     map[ebiten.GamepadID][]ebiten.StandardGamepadAxis
+func NewGame() (*Game, error) {
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		return nil, err
+	}
+
+	mplusFont, err := opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    16,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Game{
+		gamepad:   nyuuryoku.NewGamepad(),
+		mplusFont: mplusFont,
+	}, nil
 }
 
-func (g *game) Update() error {
-	if g.conn == nil {
-		g.conn = nyuuryoku.NewGamepadConnection()
-	}
-	if g.gamepad == nil {
-		g.gamepad = nyuuryoku.NewStandardGamepad(0)
-	}
-	if g.virtualConn == nil {
-		g.virtualConn = newVirtualGamepadConnection()
-	}
-	if g.names == nil {
-		g.names = make(map[ebiten.GamepadID]string)
-	}
-	if g.pressedButtons == nil {
-		g.pressedButtons = make(map[ebiten.GamepadID][]ebiten.StandardGamepadButton)
-	}
-	if g.axisValues == nil {
-		g.axisValues = make(map[ebiten.GamepadID][]ebiten.StandardGamepadAxis)
-	}
-
-	if g.connIsVirtual {
-		g.virtualConn.Update()
-	}
-
-	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
-		g.switchGamepad()
-	}
-
-	g.updatePressed()
-
+func (g *Game) Update() error {
 	return nil
 }
 
-func (g *game) updatePressed() {
-	g.connectedIDs = g.conn.AppendIDs(g.connectedIDs)
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Get connected gamepad IDs
+	var ids []ebiten.GamepadID
+	ids = g.gamepad.AppendIDs(ids)
 
-	for _, id := range g.connectedIDs {
-		g.gamepad.ID = id
-		g.names[id] = g.gamepad.Name()
-
-		buttons, ok := g.pressedButtons[id]
-		if !ok {
-			buttons = make([]ebiten.StandardGamepadButton, 0)
-		}
-		g.pressedButtons[id] = g.gamepad.AppendPressed(buttons)
-
-		axisValues, ok := g.axisValues[id]
-		if !ok {
-			axisValues = make([]ebiten.StandardGamepadAxis, 0)
-		}
-		g.axisValues[id] = g.gamepad.AxisValue(ebiten.s)
-
-	}
-}
-
-func (g *game) buttonName(button ebiten.MouseButton) string {
-	switch button {
-	case ebiten.MouseButtonLeft:
-		return "left"
-	case ebiten.MouseButtonRight:
-		return "right"
-	case ebiten.MouseButtonMiddle:
-		return "middle"
-	}
-
-	return "other button"
-}
-
-func (g *game) appendWheelLog() {
-	xoff, yoff := g.mouse.Wheel()
-	if xoff == 0 && yoff == 0 {
-		return
-	}
-	g.appendLog(fmt.Sprintf("wheel: %0.1f, %0.1f", xoff, yoff))
-}
-
-func (g *game) appendLog(line string) {
-	const max = 48
-	if len(g.log) < max {
-		g.log = append(g.log, line)
-	} else {
-		g.log[g.logIdx] = line
-		g.logIdx++
-		g.logIdx = g.logIdx % max
-	}
-}
-
-func (g *game) switchGamepad() {
-	setter := nyuuryoku.NewGamepadConnectionSetter(g.conn)
-
-	if g.connIsVirtual {
-		setter.SetDefault()
-	} else {
-		setter.SetAppendIDsFunc(g.virtualConn.appendIDs)
-	}
-
-	g.connIsVirtual = !g.connIsVirtual
-}
-
-func (g *game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.Gray{Y: 48})
-	if g.mouse == nil {
+	y := 16
+	if len(ids) == 0 {
+		text.Draw(screen, "No gamepad is connected.", g.mplusFont, 16, y, color.White)
 		return
 	}
 
-	g.drawButton(screen, image.Rect(16, 16, 80, 144), ebiten.MouseButtonLeft)
-	g.drawButton(screen, image.Rect(88, 16, 112, 80), ebiten.MouseButtonMiddle)
-	g.drawButton(screen, image.Rect(120, 16, 184, 144), ebiten.MouseButtonRight)
-
-	g.drawCursor(screen)
-
-	g.drawLog(screen)
-
-	ebitenutil.DebugPrintAt(screen, "press space to switch to virtual mouse", 10, 576)
-}
-
-func (g *game) drawButton(screen *ebiten.Image, bounds image.Rectangle, button ebiten.MouseButton) {
-	opt := ebiten.DrawImageOptions{}
-	opt.GeoM.Scale(float64(bounds.Dx()), float64(bounds.Dy()))
-	opt.GeoM.Translate(float64(bounds.Min.X), float64(bounds.Min.Y))
-	if g.mouse.Pressed(button) {
-		opt.ColorScale.SetG(0.5)
-		opt.ColorScale.SetB(0.5)
-	}
-
-	screen.DrawImage(dummyWhitePixel, &opt)
-}
-
-func (g *game) drawCursor(screen *ebiten.Image) {
-	x, y := g.mouse.CursorPosition()
-
-	opt := ebiten.DrawImageOptions{}
-	opt.GeoM.Scale(9, 9)
-	opt.GeoM.Translate(float64(x-4), float64(y-4))
-
-	screen.DrawImage(dummyWhitePixel, &opt)
-}
-
-func (g *game) drawLog(screen *ebiten.Image) {
-	for i := range g.log {
-		line := g.log[i]
-
-		yIdx := (i - g.logIdx)
-		if yIdx < 0 {
-			yIdx += len(g.log)
+	// Display information for each gamepad
+	for _, id := range ids {
+		name := g.gamepad.Name(id)
+		isStandard := g.gamepad.IsStandardLayoutAvailable(id)
+		layoutStr := "Standard"
+		if !isStandard {
+			layoutStr = "Custom"
 		}
-		y := yIdx * 12
 
-		ebitenutil.DebugPrintAt(screen, line, 200, y)
+		text.Draw(screen, fmt.Sprintf("Gamepad %d: %s (%s)", id, name, layoutStr), g.mplusFont, 16, y, color.White)
+		y += 20
+
+		// Display axis values
+		axisCount := g.gamepad.AxisCount(id)
+		for a := 0; a < axisCount; a++ {
+			value := g.gamepad.AxisValue(id, a)
+			text.Draw(screen, fmt.Sprintf("  Axis %d: %.2f", a, value), g.mplusFont, 16, y, color.White)
+			y += 16
+		}
+
+		// Display button states
+		if isStandard {
+			// For standard layout
+			buttons := []ebiten.StandardGamepadButton{
+				ebiten.StandardGamepadButtonRightBottom,
+				ebiten.StandardGamepadButtonRightRight,
+				ebiten.StandardGamepadButtonRightTop,
+				ebiten.StandardGamepadButtonRightLeft,
+			}
+			for _, b := range buttons {
+				if g.gamepad.IsStandardButtonPressed(id, b) {
+					text.Draw(screen, fmt.Sprintf("  Button %d: Pressed", b), g.mplusFont, 16, y, color.RGBA{0, 255, 0, 255})
+				} else {
+					text.Draw(screen, fmt.Sprintf("  Button %d: Released", b), g.mplusFont, 16, y, color.White)
+				}
+				y += 16
+			}
+		} else {
+			// For custom layout
+			buttonCount := g.gamepad.ButtonCount(id)
+			for b := ebiten.GamepadButton(0); b < ebiten.GamepadButton(buttonCount); b++ {
+				if g.gamepad.IsButtonPressed(id, b) {
+					text.Draw(screen, fmt.Sprintf("  Button %d: Pressed", b), g.mplusFont, 16, y, color.RGBA{0, 255, 0, 255})
+				} else {
+					text.Draw(screen, fmt.Sprintf("  Button %d: Released", b), g.mplusFont, 16, y, color.White)
+				}
+				y += 16
+			}
+		}
+
+		y += 16 // Space between gamepads
+	}
+
+	// Display newly connected gamepads
+	var justConnectedIDs []ebiten.GamepadID
+	justConnectedIDs = g.gamepad.AppendJustConnectedIDs(justConnectedIDs)
+	if len(justConnectedIDs) > 0 {
+		for _, id := range justConnectedIDs {
+			text.Draw(screen, fmt.Sprintf("New gamepad connected: %d", id), g.mplusFont, 16, y, color.RGBA{0, 255, 0, 255})
+			y += 20
+		}
 	}
 }
 
-type virtualGamepadConnection struct {
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
-func newVirtualGamepadConnection() *virtualGamepadConnection {
-	return &virtualGamepadConnection{}
-}
+func main() {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Gamepad Example")
 
-func (g *virtualGamepadConnection) Update() {
+	game, err := NewGame()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-}
-
-func (g *virtualGamepadConnection) appendIDs(gamepadIDs []ebiten.GamepadID) []ebiten.GamepadID {
-
-}
-
-type virtualStandardGamepad struct {
-}
-
-func (g *virtualStandardGamepad) name() string {
-
-}
-
-func (g *virtualStandardGamepad) axisValue(axis ebiten.StandardGamepadAxis) float64 {
-
-}
-
-func (g *virtualStandardGamepad) appendPressed(button []ebiten.StandardGamepadButton) []ebiten.StandardGamepadButton {
-
-}
-
-func (g *game) Layout(outsideWidth int, outsideHeight int) (screenWidth int, screenHeight int) {
-	return outsideWidth, outsideHeight
-}
-
-var (
-	dummyImageBase = ebiten.NewImage(3, 3)
-
-	// dummyWhitePixel is a 1x1 white pixel image.
-	dummyWhitePixel = dummyImageBase.SubImage(image.Rect(1, 1, 2, 2)).(*ebiten.Image)
-)
-
-func init() {
-	dummyImageBase.Fill(color.White)
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.3
 
 require (
 	github.com/hajimehoshi/ebiten/v2 v2.8.3
+	golang.org/x/image v0.20.0
 	golang.org/x/tools v0.25.0
 )
 
@@ -15,4 +16,5 @@ require (
 	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
